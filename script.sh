41b0a90bc74526fc2aab62e85e6455d7650c8328
Motoaki Sasaki <m.sasaki@vwall.co.jp>
2024-06-07 11:29:11 +0900

    用意されたemfを使ってみる

--- script.sh
+++ script.sh
@@ -3,10 +3,11 @@
 TEMP_PATH="$(mktemp -d)"
 PATH="${TEMP_PATH}:$PATH"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-STYLELINT_FORMATTER="${GITHUB_ACTION_PATH}/stylelint-formatter-rdjson/index.js"
+# STYLELINT_FORMATTER="${GITHUB_ACTION_PATH}/stylelint-formatter-rdjson/index.js"
 
 __run_stylelint() {
-  cmd="stylelint ${INPUT_STYLELINT_INPUT} --custom-formatter ${STYLELINT_FORMATTER}"
+  cmd="stylelint ${INPUT_STYLELINT_INPUT}
+  #  --custom-formatter ${STYLELINT_FORMATTER}"
 
   if [ -n "${INPUT_STYLELINT_CONFIG}" ]; then
     cmd="${cmd} --config='${INPUT_STYLELINT_CONFIG}'"
@@ -20,7 +21,7 @@ __run_stylelint() {
 }
 
 __run_reviewdog() {
-  reviewdog -f=rdjson \
+  reviewdog -f=stylelint \
             -name="${INPUT_NAME}" \
             -reporter="${INPUT_REPORTER}" \
             -level="${INPUT_LEVEL}" \
